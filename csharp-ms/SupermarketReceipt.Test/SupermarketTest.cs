a30fa8f96d3f380313a0e2a0df0a39ee0aec7ced
Christian Peters <christian.peters@gmail.com>
2024-08-24 10:39:40 -0500

    One way to do tests - 20 mins

--- csharp-ms/SupermarketReceipt.Test/SupermarketTest.cs
+++ csharp-ms/SupermarketReceipt.Test/SupermarketTest.cs
@@ -9,18 +9,63 @@ namespace Supermarket.Test
     [TestClass]
     public class SupermarketTest
     {
+        SupermarketCatalog catalog;
+        Product toothbrush;
+        Product apples;
+        Product rice;
+
+        [TestInitialize]
+        public void BeforeEachTest()
+        {
+            catalog = new FakeCatalog();
+            toothbrush = new Product("toothbrush", ProductUnit.Each);
+            rice = new Product("rice", ProductUnit.Each);
+            apples = new Product("apples", ProductUnit.Kilo);
+        }
+
+        private ShoppingCart SetupStandardCart()
+        {
+            catalog.AddProduct(toothbrush, 1);
+            catalog.AddProduct(apples, 2);
+            catalog.AddProduct(rice, 1);
+
+            var cart = new ShoppingCart();
+            cart.AddItemQuantity(apples, 2.5);
+            cart.AddItemQuantity(toothbrush, 1);
+            cart.AddItemQuantity(rice, 3);
+
+            return cart;
+        }
+
+        [TestMethod]
+        public void NoDiscounts()
+        {
+            // ARRANGE
+            var cart = SetupStandardCart();
+            var teller = new Teller(catalog);
+            // ACT
+            var receipt = teller.ChecksOutArticlesFrom(cart);
+
+            // ASSERT
+            VerifyTotals(receipt, 9, 0, 3);
+
+            VerifyItem(receipt, apples, 0, 2, 5, 2.5);
+            VerifyItem(receipt, toothbrush, 1, 1, 1, 1);
+            VerifyItem(receipt, rice, 2, 1, 3, 3);
+        }
+
+        private static void VerifyTotals(Receipt receipt, double totalPrice, int discountCount, int itemCount)
+        {
+            receipt.GetTotalPrice().Should().Be(totalPrice);
+            receipt.GetDiscounts().Count.Should().Be(discountCount);
+            receipt.GetItems().Count.Should().Be(itemCount);
+        }
+
         [TestMethod]
         public void TenPercentDiscount()
         {
             // ARRANGE
-            SupermarketCatalog catalog = new FakeCatalog();
-            var toothbrush = new Product("toothbrush", ProductUnit.Each);
-            catalog.AddProduct(toothbrush, 0.99);
-            var apples = new Product("apples", ProductUnit.Kilo);
-            catalog.AddProduct(apples, 1.99);
-
-            var cart = new ShoppingCart();
-            cart.AddItemQuantity(apples, 2.5);
+            var cart = SetupStandardCart();
 
             var teller = new Teller(catalog);
             teller.AddSpecialOffer(SpecialOfferType.TenPercentDiscount, toothbrush, 10.0);
@@ -29,15 +74,42 @@ namespace Supermarket.Test
             var receipt = teller.ChecksOutArticlesFrom(cart);
 
             // ASSERT
-            receipt.GetTotalPrice().Should().Be(4.975);
-            receipt.GetDiscounts().Should().BeEmpty();
-            receipt.GetItems().Count.Should().Be(1);
+            VerifyTotals(receipt, 8.9, 1, 3);
+            VerifyDiscount(receipt, toothbrush, 0, "10% off", -0.1);
+        }
 
-            var receiptItem = receipt.GetItems()[0];
-            receiptItem.Product.Should().Be(apples);
-            receiptItem.Price.Should().Be(1.99);
-            receiptItem.TotalPrice.Should().Be(2.5 * 1.99);
-            receiptItem.Quantity.Should().Be(2.5);
+        [TestMethod]
+        public void ThreeForTwoDiscount()
+        {
+            // ARRANGE
+            var cart = SetupStandardCart();
+
+            var teller = new Teller(catalog);
+            teller.AddSpecialOffer(SpecialOfferType.ThreeForTwo, rice, 1);
+
+            // ACT
+            var receipt = teller.ChecksOutArticlesFrom(cart);
+
+            // ASSERT
+            VerifyTotals(receipt, 8, 1, 3);
+            VerifyDiscount(receipt, rice, 0, "3 for 2", -1);
+        }
+
+        private static void VerifyDiscount(Receipt receipt, Product product, int index, string description, double discountAmount)
+        {
+            var discount = receipt.GetDiscounts()[index];
+            discount.Product.Should().Be(product);
+            discount.Description.Should().Be(description);
+            discount.DiscountAmount.Should().Be(discountAmount);
+        }
+
+        private static void VerifyItem(Receipt receipt, Product product, int index, double perPrice, double totalPrice, double quantity)
+        {
+            var receiptItem = receipt.GetItems()[index];
+            receiptItem.Product.Should().Be(product);
+            receiptItem.Price.Should().Be(perPrice);
+            receiptItem.TotalPrice.Should().Be(totalPrice);
+            receiptItem.Quantity.Should().Be(quantity);
         }
     }
 }
